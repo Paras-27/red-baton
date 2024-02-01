694a2a350e5e8e1708bb6de96c86bffa283d1996
Paras <parasbupadhyay2003@gmail.com>
2024-02-02 00:05:55 +0530

    initial commit

---
+++ server/src/controllers/news.controller.js
@@ -0,0 +1,14 @@
+import { News } from "../models/news.models.js";
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+
+export const fetchNews = async (req, res) => {
+    try {
+      const data = await News.find().sort({ posted_on: -1 });
+  
+      res.status(200).json(new ApiResponse(200, data, "success"));
+    } catch (error) {
+      throw new ApiError(404, "Not Found");
+    }
+  };
+  
